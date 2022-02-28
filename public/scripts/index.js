1a71420efa9477d265f484ed926f9c2d098a0ca4
Anca Puscasu <ancapuscasu@ymail.com>
2022-02-28 12:19:32 -0500

    reorganized folder structure for deployment

---
+++ public/scripts/index.js
@@ -0,0 +1,49 @@
+function validateForm() {
+    var n = document.getElementById('name').value;
+    var e = document.getElementById('email').value;
+    var m = document.getElementById('message').value;
+    var onlyEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    
+    
+    if(n == "" || n == null){
+        document.getElementById('nameLabel').innerHTML = ('Please enter your name');
+        document.getElementById('name').style.borderColor = "red";
+        return false;
+    }
+       
+  
+    if(e == "" || e == null ){
+          document.getElementById('emailLabel').innerHTML = ('Please enter your email');
+          document.getElementById('email').style.borderColor = "red";
+          return false;
+      }
+  
+    if (!e.match(onlyEmail)) {
+        document.getElementById('emailLabel').innerHTML = ('Please enter a valid email address');
+        document.getElementById('email').style.borderColor = "red";
+        return false;
+    }
+  
+  
+    if(m == "" || m == null){
+        document.getElementById('messageLabel').innerHTML = ('Please enter your message');
+        document.getElementById('message').style.borderColor = "red";
+        return false;
+    }
+  
+    else{
+          return true;
+      }
+      
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
